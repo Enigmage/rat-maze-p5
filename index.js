8c4ac565e3a0143a99dd2a1fb7627daab9bcdfcb
Enigmage <alizaidi0786.az@gmail.com>
2023-06-27 20:57:01 +0530

    first commit

--- index.js
+++ index.js
@@ -1,72 +1,153 @@
+const sleep = ms => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+const create2DArray = (rows, cols) => {
+  let arr = new Array(cols);
+  for (let i = 0; i < cols; i++) arr[i] = new Array(rows);
+  return arr;
+};
+
 const s = p => {
-  let dim, rows, cols, maze;
-  let rx, ry;
-  const create2DArray = (rows, cols) => {
-    let arr = new Array(cols);
-    for (let i = 0; i < cols; i++) arr[i] = new Array(rows);
-    return arr;
-  };
+  let dim,
+    rows,
+    cols,
+    maze,
+    simFinished,
+    simSpeed,
+    mazeValues,
+    directions,
+    vis,
+    simStarted;
   p.setup = () => {
-    dim = 40;
-    let myCanvas = p.createCanvas(800, 400);
-    p.frameRate(5);
-    myCanvas.parent("my-canvas");
-    // p.background("black");
+    let myCanvas = p.createCanvas(800, 400).parent("my-canvas");
+    myCanvas.mousePressed(mousePressed);
+    dim = 50;
     rows = p.floor(p.height / dim);
     cols = p.floor(p.width / dim);
+    simSpeed = 100; // in ms
+    mazeValues = {
+      ratTrail: 1,
+      obstacle: 2,
+      finish: 3,
+      clearPath: 4,
+      // usedPath: 5,
+    };
+    directions = [
+      [1, 0],
+      [0, 1],
+      [-1, 0],
+      [0, -1],
+      // [1, 1],
+      // [-1, -1],
+      // [1, -1],
+      // [-1, 1],
+    ];
+    simStarted = false;
+    simFinished = false;
     maze = create2DArray(rows, cols);
-    rx = 0;
-    ry = 0;
-    init();
+    vis = create2DArray(rows, cols);
+    initGridState();
   };
   p.draw = () => {
-    paint();
-  };
-
-  const paint = () => {
     for (let i = 0; i < cols; i++) {
       for (let j = 0; j < rows; j++) {
-        if (maze[i][j] === 1) p.fill("white");
-        else if (maze[i][j] === 2) p.fill("yellow");
-        else if (maze[i][j] === 3) p.fill("black");
-        p.stroke(100);
-        p.rect(i * dim, j * dim, dim - 2, dim - 2);
+        switch (maze[i][j]) {
+          case mazeValues.ratTrail:
+            p.fill("#ff0");
+            break;
+          case mazeValues.obstacle:
+            p.fill("#000");
+            break;
+          case mazeValues.finish:
+            p.fill("#03fc7f");
+            break;
+          // case mazeValues.usedPath:
+          //   p.fill("#f11");
+          //   break;
+          case mazeValues.clearPath:
+          default:
+            p.fill("#fff");
+        }
+        p.stroke(160);
+        p.rect(i * dim, j * dim, dim - 3, dim - 3);
       }
     }
   };
-
-  const init = () => {
-    for (let i = 0; i < cols; i++) {
-      for (let j = 0; j < rows; j++) {
-        maze[i][j] = 1;
-      }
-    }
-    maze[2][2] = 3;
-    maze[19][2] = 3;
-    maze[19][8] = 3;
-    maze[13][8] = 3;
-    maze[0][0] = 2;
-  };
-  // backtracking algorithm goes here...
-  const generate = () => {
+  const ratInMaze = async (x, y) => {
     if (
-      rx < 0 ||
-      rx == cols ||
-      ry < 0 ||
-      ry == rows ||
-      maze[rx][ry] === 3 ||
-      maze[rx][ry] === 2
-    )
-      return;
-    if (rx === cols - 1 && ry === rows - 1) return;
-    maze[rx][ry] = 2;
-    generate(rx + 1, ry);
-    generate(rx - 1, ry);
-    generate(rx, ry + 1);
-    generate(rx, ry - 1);
-    maze[rx][ry] = 1;
-    return;
+      x >= 0 &&
+      x < cols &&
+      y >= 0 &&
+      y < rows &&
+      maze[x][y] === mazeValues.clearPath &&
+      vis[x][y] === false
+    ) {
+      vis[x][y] = true;
+      if (x == cols - 1 && y == rows - 1) {
+        maze[x][y] = mazeValues.finish;
+        simFinished = true;
+        return;
+      }
+      // boundary
+      if (simFinished) return;
+
+      maze[x][y] = mazeValues.ratTrail;
+      await sleep(simSpeed);
+
+      for (let [dx, dy] of directions) await ratInMaze(x + dx, y + dy);
+
+      // boundary
+      if (simFinished) return;
+
+      maze[x][y] = mazeValues.clearPath;
+      await sleep(simSpeed);
+    }
   };
+
+  const initGridState = (preserveObstacles = false) => {
+    simFinished = false;
+    for (let i = 0; i < cols; i++) {
+      for (let j = 0; j < rows; j++) {
+        preserveObstacles
+          ? (maze[i][j] =
+            maze[i][j] == mazeValues.obstacle
+              ? maze[i][j]
+              : mazeValues.clearPath)
+          : (maze[i][j] = mazeValues.clearPath);
+        vis[i][j] = false;
+      }
+    }
+  };
+
+  const mousePressed = () => {
+    if (simStarted) return;
+    let col = (p.mouseX - (p.mouseX % dim)) / dim;
+    let row = (p.mouseY - (p.mouseY % dim)) / dim;
+
+    if (row < rows && row >= 0 && col < cols && col >= 0) {
+      maze[col][row] =
+        maze[col][row] === maze.clearPath
+          ? mazeValues.obstacle
+          : maze.clearPath;
+    }
+  };
+
+  document.getElementById("startBtn").addEventListener("click", async e => {
+    e.preventDefault();
+    if (simStarted || simFinished) return;
+    simStarted = true;
+    await ratInMaze(0, 0);
+    simStarted = false;
+  });
+  document.getElementById("resetBtn").addEventListener("click", e => {
+    e.preventDefault();
+    if (!simStarted) initGridState(true);
+  });
+  document.getElementById("clearBtn").addEventListener("click", e => {
+    e.preventDefault();
+    if (!simStarted) initGridState();
+  });
 };
 
 let sketch = new p5(s);
